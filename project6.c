57fba6c2f04c8f02c6d5977c3903c8c275debc99
trupti652 <75348537+trupti652@users.noreply.github.com>
2020-12-14 20:48:54 +0530

    Add files via upload

---
+++ project6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float perimiter;
+    float Area;
+    int radius=6;
+
+
+     perimiter=2*3.14*radius;
+    printf("the perimiter of circle =%f inches\n",perimiter);
+
+
+      Area=3.14*radius*radius;
+    printf("the area circle = %f square inches\n",Area);
+}
